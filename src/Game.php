4526e35c2a9ab980046b5e677b106657c1da6d12
Rocco Palladino <rocky@uchicago.edu>
2014-04-17 23:36:20 -0500

    Added dockblock comments

--- src/Game.php
+++ src/Game.php
@@ -4,13 +4,23 @@ namespace BowlingGame;
 
 class Game
 {
+	/**
+	 * Called each time a player rolls a ball. 
+	 * 
+	 * @param  int $pins The number of pins knocked down.
+	 * @return void
+	 */
 	public function roll($pins)
 	{
 
 	}
 
+	/**
+	 * Called only at the very end of the game.
+	 * @return int Total score for the game.
+	 */
 	public function score()
 	{
-		
+
 	}
 }
