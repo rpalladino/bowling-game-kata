a9ffc8e21df4602fd83af0500cab5c5424952dbe
Rocco Palladino <rocky@uchicago.edu>
2014-04-18 11:05:46 -0500

    The Third test.

--- src/Game.php
+++ src/Game.php
@@ -4,25 +4,43 @@ namespace BowlingGame;
 
 class Game
 {
-	private $score = 0;
+    private $rolls = array();
+    private $currentRoll = 0;
 
-	/**
-	 * Called each time a player rolls a ball. 
-	 * 
-	 * @param  int $pins The number of pins knocked down.
-	 * @return void
-	 */
-	public function roll($pins)
-	{
-		$this->score += $pins;
-	}
+    /**
+     * Called each time a player rolls a ball. 
+     * 
+     * @param  int $pins The number of pins knocked down.
+     * @return void
+     */
+    public function roll($pins)
+    {
+        $this->rolls[$this->currentRoll++] = $pins;
+    }
 
-	/**
-	 * Called only at the very end of the game.
-	 * @return int Total score for the game.
-	 */
-	public function score()
-	{
-		return $this->score;
-	}
+    /**
+     * Called only at the very end of the game.
+     * @return int Total score for the game.
+     */
+    public function score()
+    {
+        $score = 0;
+        $frameIndex = 0;
+        for ($frame = 0; $frame < 10; $frame++) {
+            if ($this->isSpare($frameIndex))
+            {
+                $score += 10 + $this->rolls[$frameIndex + 2];
+                $frameIndex += 2;
+            } else {
+                $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+                $frameIndex += 2;
+            }
+        }
+        return $score;
+    }
+
+    private function isSpare($frameIndex)
+    {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1] == 10;
+    }
 }
--- test/GameTest.php
+++ test/GameTest.php
@@ -6,34 +6,49 @@ require __DIR__ . '/../src/Game.php';
 
 class GameTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @var [Game
-	 */
-	private $g;
+    /**
+     * @var [Game
+     */
+    private $g;
 
-	public function setUp()
-	{
-		$this->g = new Game;
-	}
+    public function setUp()
+    {
+        $this->g = new Game;
+    }
 
-	private function rollMany($n, $pins)
-	{
-		for ($i=0; $i<$n; $i++) {
-			$this->g->roll($pins);
-		}
-	}
+    private function rollMany($n, $pins)
+    {
+        for ($i=0; $i<$n; $i++) {
+            $this->g->roll($pins);
+        }
+    }
 
-	public function testGutterGame()
-	{
-		$n = 20;
-		$pins = 0;
-		$this->rollMany(20, 0);
-		$this->assertEquals(0, $this->g->score());
-	}
+    private function rollSpare()
+    {
+        $this->g->roll(5);
+        $this->g->roll(5);
+    }
 
-	public function testAllOnes()
-	{
-		$this->rollMany(20, 1);
-		$this->assertEquals(20, $this->g->score());
-	}
+    public function testGutterGame()
+    {
+        $n = 20;
+        $pins = 0;
+        $this->rollMany(20, 0);
+        $this->assertEquals(0, $this->g->score());
+    }
+
+    public function testAllOnes()
+    {
+        $this->rollMany(20, 1);
+        $this->assertEquals(20, $this->g->score());
+    }
+
+    public function testOneSpare()
+    {
+        $this->rollSpare();
+        $this->g->roll(3);
+        $this->rollMany(17,0);
+        $this->assertEquals(16, $this->g->score());
+    }
+
 }
